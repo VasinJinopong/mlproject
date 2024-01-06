f6997e1ec0a14a632be8c5540cd7b5a02163d7cb
VasinJinopong <vasinearth14@gmail.com>
2024-01-06 19:44:17 +0700

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts','train.csv')
@@ -44,4 +50,14 @@ class DataIngestion:
         
 if __name__== "__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    # print(f"len_train_arr : {train_arr.shape} , len_test_arr : {test_arr.shape}")
+    # X_train,y_train,X_test,y_test = modeltrainer.initiate_model_trainer(train_arr,test_arr)
+    # print(f"len_X_train : {X_train.shape} len_y_train : {y_train.shape} len_X_test : {X_test.shape} len_y_test : {y_test.shape}")
+
