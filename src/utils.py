eddbc07e7c20f696f9d39ed9efca9e6ba6405cff
VasinJinopong <vasinearth14@gmail.com>
2024-01-08 20:54:13 +0700

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 
 def save_object(file_path,obj):
@@ -22,14 +22,23 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
     
 
-def evaluate_models(X_train,y_train, X_test,y_test,models):
+def evaluate_models(X_train,y_train, X_test,y_test,models,param):
     try:
         report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
+        # for i in range(len(list(models))):
+        #     model = list(models.values())[i]
+        #     para = param[list(models.keys())[i]]
+        for model_name, model in models.items():
+            # Use the correct model name to access parameters
+            para = param[model_name]
+
+            gs = GridSearchCV(model,para, cv=3,refit=True)
+            gs.fit(X_train,y_train)
             
-            model.fit(X_train, y_train) # Train model
+            # model.fit(X_train, y_train) # Train model
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
 
@@ -39,7 +48,10 @@ def evaluate_models(X_train,y_train, X_test,y_test,models):
 
             test_model_score = r2_score(y_test,y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            # report[list(models.keys())[i]] = test_model_score
+            report[model_name] = test_model_score
+            
+
 
         return report
     
